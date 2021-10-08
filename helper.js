dacc44b6487846c189c7b9b6eb5b6ab02cb56ce7
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-08 14:38:12 +0530

    ZᴀʀͥᴀͭᴍᷤWᷟᴏͤʟ

--- helper.js
+++ helper.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *pinky*:  ```" + msg + "```"
+    return "✅ *Zara*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *pinky*:  ```" + msg + "```"
+    return "🛑 *Zara*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *pinky*:  ```" + msg + "```"
+    return "⏺️ *Zara*:  ```" + msg + "```"
 }
 
 
