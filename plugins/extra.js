bfe251ea9176b1fae9c039a7c587d0455eff488a
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-09 10:00:29 +0530

    ZᴀʀͥᴀͭᴍᷤWᷟᴏͤʟ

--- plugins/extra.js
+++ plugins/extra.js
@@ -1,4 +1,4 @@
- const Sophia = require('../events');
+const Rashi = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 const axios = require('axios');
@@ -9,7 +9,7 @@ const Lang = Language.getString('weather');
 
 if (config.WORKTYPE == 'private') {
 
-    Sophia.tosophia({pattern: 'sweather ?(.*)', fromMe: true, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
+    Rashi.tozara({pattern: 'sweather ?(.*)', fromMe: true, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
 	  if (match[1] === 'where is it?') return await message.reply("wait...");
 	  const url = `https://api.nasa.gov/DONKI/notifications?type=all&api_key=ccdRSkRerDoJHn2g36DGnJgyctfkGDml3ZzMWWxL`;
 	  try {
@@ -31,7 +31,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 	
-    Sophia.tosophia({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
+    Rashi.tozara({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
@@ -49,7 +49,7 @@ if (config.WORKTYPE == 'private') {
 	    }
     });
 
-    Sophia.tosophia({pattern: 'compliment ?(.*)', fromMe: true, desc: Lang.CM_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'compliment ?(.*)', fromMe: true, desc: Lang.CM_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://complimentr.com/api`;
 	  try {
@@ -61,7 +61,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    Sophia.tosophia({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+    Rashi.tozara({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 	  if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	  const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
 	  try {
@@ -76,7 +76,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    Sophia.tosophia({pattern: 'insult ?(.*)', fromMe: true, desc: Lang.EVINS_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'insult ?(.*)', fromMe: true, desc: Lang.EVINS_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://evilinsult.com/generate_insult.php?lang=en&type=json`;
 	  try {
@@ -88,7 +88,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    Sophia.tosophia({ pattern: 'movie ?(.*)', fromMe: true, desc: "Shows movie info." }, (async (message, match) => {
+    Rashi.tozara({pattern: 'movie ?(.*)', fromMe: true, desc: "Shows movie info." }, (async (message, match) => {
 	  if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	  let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
 	  const response = await got(url);
@@ -115,7 +115,7 @@ if (config.WORKTYPE == 'private') {
 	  await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
   }));
 
-    Sophia.tosophia({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://official-joke-api.appspot.com/random_joke`;
 	  try {
@@ -128,7 +128,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    Sophia.tosophia({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'zara ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
@@ -140,22 +140,9 @@ if (config.WORKTYPE == 'private') {
 	}
     });
 
-    Sophia.tosophia({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+    
 
-        if (message.jid === '15369524516-1612300121@g.us') {
-
-            return;
-        }
-
-        if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'ᴘɪɴᴋʏ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀꜰɴᴀɴᴘʟᴋ' , MessageType.text, { quoted: message.data });
-        }
-        else {
-            await message.client.sendMessage(message.jid,config.PLK, MessageType.text, { quoted: message.data });
-        }
-    }));
-
-    Sophia.tosophia({pattern: 'quote ?(.*)', fromMe: true, desc: Lang.QUOTE_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'quote ?(.*)', fromMe: true, desc: Lang.QUOTE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://api.quotable.io/random`;
 	  try {
@@ -171,7 +158,7 @@ if (config.WORKTYPE == 'private') {
 }
 else if (config.WORKTYPE == 'public') {
 
-    Sophia.tosophia({pattern: 'sweather ?(.*)', fromMe: false, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
+    Rashi.tozara({pattern: 'sweather ?(.*)', fromMe: false, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
 	  if (match[1] === 'where is it?') return await message.reply("wait...");
 	  const url = `https://api.nasa.gov/DONKI/notifications?type=all&api_key=ccdRSkRerDoJHn2g36DGnJgyctfkGDml3ZzMWWxL`;
 	  try {
@@ -193,7 +180,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 	
-   Sophia.tosophia({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
+   Rashi.tozara({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
@@ -211,7 +198,7 @@ else if (config.WORKTYPE == 'public') {
 	    }
     });
 
-    Sophia.tosophia({pattern: 'compliment ?(.*)', fromMe: false, desc: Lang.CM_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'compliment ?(.*)', fromMe: false, desc: Lang.CM_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://complimentr.com/api`;
 	  try {
@@ -223,7 +210,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    Sophia.tosophia({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+    Rashi.tozara({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 	  if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	  const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
 	  try {
@@ -238,7 +225,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    Sophia.tosophia({pattern: 'insult ?(.*)', fromMe: false, desc: Lang.EVINS_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'insult ?(.*)', fromMe: false, desc: Lang.EVINS_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://evilinsult.com/generate_insult.php?lang=en&type=json`;
 	  try {
@@ -250,7 +237,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    Sophia.tosophia({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
+    Rashi.tozara({pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
 	  if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	  let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
 	  const response = await got(url);
@@ -277,7 +264,7 @@ else if (config.WORKTYPE == 'public') {
 	  await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
   }));
 
-    Sophia.tosophia({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://official-joke-api.appspot.com/random_joke`;
 	  try {
@@ -290,7 +277,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    Sophia.tosophia({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'zara ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
@@ -302,22 +289,9 @@ else if (config.WORKTYPE == 'public') {
 	}
     });
 
-    Sophia.tosophia({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+    
 
-        if (message.jid === '15369524516-1612300121@g.us') {
-
-            return;
-        }
-
-        if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'ᴘɪɴᴋʏ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀꜰɴᴀɴᴘʟᴋ' , MessageType.text, { quoted: message.data });
-        }
-        else {
-            await message.client.sendMessage(message.jid,config.PLK, MessageType.text, { quoted: message.data });
-        }
-    }));
-
-    Sophia.tosophia({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
+    Rashi.tozara({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://api.quotable.io/random`;
 	  try {
