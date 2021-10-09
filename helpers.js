466735ffeb848a4a4ac0ba60d98bf688c8087766
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-09 11:23:23 +0530

    ZᴀʀͥᴀͭᴍᷤWᷟᴏͤʟ

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 /*
 # Copyright (C) 2020 MuhammedKpln.
-#
+# 
 # WhatsAsena is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
