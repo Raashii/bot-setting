36ac25dfc3ea594ebb3cbb2cc58637f0878d4de4
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-18 11:47:34 +0530

    ```ZaraMwol 2.5 💖```

---
+++ plugins/cont.js
@@ -0,0 +1,43 @@
+/* # Exclusively from raashii
+ */ 
+
+const Rashi = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../zara');
+const config = require('../config');
+
+if (config.WORKTYPE == 'private') {
+
+Rashi.tozara({pattern: 'number', fromMe: true, desc: 'Its send owner number'}, (async (message, match) => {
+
+            const Raashii = 'BEGIN:VCARD\n'
+            + 'VERSION:3.0\n' 
+            + 'FN:' + Config.Z_OWNER + '\n' //created afnanplk, please copy this with credit..
+            + 'ORG:Zara fam;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=' + Config.Z_NUMBER + ':' + Config.Z_NUMBER + ' \n'
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid, {displayname: "Owner", vcard: Raashii}, MessageType.contact);
+
+  }));
+}
+
+else if (config.WORKTYPE == 'public') {
+
+Rashi.tozara({pattern: 'number', fromMe: false, desc: 'Its send owner number'}, (async (message, match) => {
+         var mode = ''
+if (Config.Z_OWNER == 'Raashii') mode = 'ᴅᴇᴠᴇʟᴏᴘᴇʀ : '
+
+else mode = 'ᴏᴡɴᴇʀ : '
+            
+            const Raashii = 'BEGIN:VCARD\n'
+            + 'VERSION:3.0\n' 
+            + 'FN:' + mode + Config.Z_OWNER + '\n' //created afnanplk, please copy this with credit..
+            + 'ORG:Zara fam;\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=' + Config.Z_NUMBER + ':' + Config.Z_NUMBER + ' \n'
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid, {displayname: "Owner", vcard: Raashii}, MessageType.contact);
+
+  }));
+}
