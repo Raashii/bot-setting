278190880709e178bff52c8e7e6a60dbc676c5ab
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-18 11:51:59 +0530

    ```ZaraMwol 2.5 💖```

--- plugins/cont.js
+++ plugins/cont.js
@@ -14,9 +14,9 @@ Rashi.tozara({pattern: 'number', fromMe: true, desc: 'Its send owner number'}, (
 
             const Raashii = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
-            + 'FN:' + Config.Z_OWNER + '\n' //created afnanplk, please copy this with credit..
+            + 'FN:' + Config.OA_NAME + '\n' //created afnanplk, please copy this with credit..
             + 'ORG:Zara fam;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=' + Config.Z_NUMBER + ':' + Config.Z_NUMBER + ' \n'
+            + 'TEL;type=CELL;type=VOICE;waid=' + Config.PHONE + ':' + Config.PHONE + ' \n'
             + 'END:VCARD'
 await message.client.sendMessage(message.jid, {displayname: "Owner", vcard: Raashii}, MessageType.contact);
 
@@ -27,15 +27,15 @@ else if (config.WORKTYPE == 'public') {
 
 Rashi.tozara({pattern: 'number', fromMe: false, desc: 'Its send owner number'}, (async (message, match) => {
          var mode = ''
-if (Config.Z_OWNER == 'Raashii') mode = 'ᴅᴇᴠᴇʟᴏᴘᴇʀ : '
+if (Config.OA_NAME == 'Raashii') mode = 'ᴅᴇᴠᴇʟᴏᴘᴇʀ : '
 
 else mode = 'ᴏᴡɴᴇʀ : '
             
             const Raashii = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
-            + 'FN:' + mode + Config.Z_OWNER + '\n' //created afnanplk, please copy this with credit..
+            + 'FN:' + mode + Config.OA_NAME + '\n' //created afnanplk, please copy this with credit..
             + 'ORG:Zara fam;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=' + Config.Z_NUMBER + ':' + Config.Z_NUMBER + ' \n'
+            + 'TEL;type=CELL;type=VOICE;waid=' + Config.PHONE + ':' + Config.PHONE + ' \n'
             + 'END:VCARD'
 await message.client.sendMessage(message.jid, {displayname: "Owner", vcard: Raashii}, MessageType.contact);
 
