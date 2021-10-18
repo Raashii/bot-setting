9c431405ed55ca0fcbd13e0a7f563c90a4bc121b
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-18 15:56:18 +0530

    ```ZaraMwol 2.5 💖```

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -7,6 +7,7 @@ const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const translatte = require('translatte');
 const config = require('../config');
 const Pinky = require('../zara');
+const snd = require('../zara');
 const LanguageDetect = require('languagedetect');
 const lngDetector = new LanguageDetect();
 const Heroku = require('heroku-client');
@@ -825,7 +826,43 @@ else if (config.WORKTYPE == 'public') {
             voice: LANG
         });
         await message.client.sendMessage(message.jid,buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio,quoted: message.data,  ptt: true});
-    }));
+   DOWN Rashi.tozara({ pattern: 'play ?(.*)', fromMe: false, desc: 'it send song'}, (async (message, match) => {
+
+  if (match[1] === '') return await message.client.sendMessage(message.jid, 'NEED WORD 🍁', MessageType.text, { quoted: message.data });
+  let arama = await yts(match[1]);
+  arama = arama.all;
+  if (arama.length < 1) return await message.client.sendMessage(message.jid, Lang.NO_RESULT, MessageType.text, { quoted: message.data });
+  var reply = await message.client.sendMessage(message.jid, snd.SONGD, MessageType.text, { quoted: message.data });
+
+  let title = arama[0].title.replace(' ', '+');
+  let stream = ytdl(arama[0].videoId, {
+    quality: 'highestaudio',
+  });
+
+  got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
+  ffmpeg(stream)
+    .audioBitrate(320)
+    .save('./' + title + '.mp3')
+    .on('end', async () => {
+      const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
+      writer.setFrame('TIT2', arama[0].title)
+        .setFrame('TPE1', [arama[0].author.name])
+        .setFrame('APIC', {
+          type: 3,
+          data: fs.readFileSync(title + '.jpg'),
+          description: arama[0].description
+        });
+      writer.addTag();
+
+      reply = await message.client.sendMessage(message.jid, fs.readFileSync('./' + title + '.jpg'), MessageType.image, { caption: '\n```Song Name :\n' + title + ' ```\n\n' + `╭──────•◈•──────╮ 
+   ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ꜱᴏɴɢ♫
+       ılı.lıllılı.ıllı.ılı.lıllılı.ıllı.
+      ━━━━━ •♬• ━━━━━
+ ╰──────•◈•──────╯
+﹏﹏﹏﹏﹏✪✭✪﹏﹏﹏﹏﹏`+ '\n' });
+      await message.client.sendMessage(message.jid, Buffer.from(writer.arrayBuffer), MessageType.document, { filename: title + '.mp3', mimetype: 'audio/mpeg', contextInfo: { forwardingScore: 1, isForwarded: false }, quoted: message.data });
+    });
+}));
 
     Rashi.tozara({pattern: 'song ?(.*)', fromMe: false, desc: Lang.SONG_DESC}, (async (message, match) => { 
 
@@ -833,7 +870,7 @@ else if (config.WORKTYPE == 'public') {
         let arama = await yts(match[1]);
         arama = arama.all;
         if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-        var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_SONG,MessageType.text);
+        var reply = await message.client.sendMessage(message.jid,snd.SONGD,MessageType.text);
 
         let title = arama[0].title.replace(' ', '+');
         let stream = ytdl(arama[0].videoId, {
@@ -855,7 +892,7 @@ else if (config.WORKTYPE == 'public') {
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG,MessageType.text);
+                reply = await message.client.sendMessage(message.jid,snd.SONGU,MessageType.text);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {quoted: message.data , mimetype: Mimetype.mp4Audio, ptt: false});
             });
     }));
