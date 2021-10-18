77ecacc82fa3451e6e848ef1086504da14995f59
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-18 11:44:56 +0530

    ```ZaraMwol 2.5 💖```

---
+++ plugins/play.js
@@ -0,0 +1,32 @@
+const Asena = require('../events');
+const { MessageType } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+const LOAD_ING = "*ᴜᴘʟᴏᴀᴅɪɴɢ...*"
+const axios = require('axios')
+
+const conf = require('../config');
+let wk = conf.WORKTYPE == 'public' ? false : true
+
+Asena.tozara({pattern: 'play ?(.*)', fromMe: wk, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+	
+        const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=Raashii`)
+	
+        const { status, result } = data
+
+	var img = await Axios.get(`${result.thumb}`, {responseType: 'arraybuffer'})
+
+        if(!status) return await message.sendMessage('*NO RESULT FOUND🥲*')
+	
+        await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
+        let msg = '```'
+        msg +=  `TITLE :${result.title}\n\n`
+        msg +=  `THUMBNAIL :${result.thumb}\n\n`
+        msg +=  `CHANNEL :${result.channel}\n\n`
+        msg +=  `DATE OF PUBLISHED :${result.published}\n\n`
+        msg +=  `TOTAL VIEWS :${result.views}\n\n`
+        msg +=  `DOWNLOADING LINK :${result.url}\n\n`
+        msg += '```'
+         return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.image, {mimetype: Mimetype.jpg , caption: msg })
+        });
+    
