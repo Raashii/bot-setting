47793006a87ed14736cba12f608ddc5177df777e
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-18 10:50:34 +0530

    ```Adding old Files```

---
+++ plugins/barcode.js
@@ -0,0 +1,18 @@
+const kannappi = require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "*type some word after command🥲*"
+let wk = conf.WORKTYPE == 'public' ? false : true
+
+    kannappi.tozara({ pattern: 'barcode ?(.*)', fromMe: wk,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/barcode_maker?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.AFN })
+
+    }));
+
