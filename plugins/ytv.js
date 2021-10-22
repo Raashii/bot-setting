70b55377e7949d429046f1d3108e7288064e73a8
Raashii <87118150+Raashii@users.noreply.github.com>
2021-10-22 17:38:17 +0530

    ```.ytv fixed```

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -14,22 +14,22 @@ let wk = conf.WORKTYPE == 'public' ? false : true
 
 Asena.tozara({pattern: 'ytv ?(.*)', fromMe: wk, desc: 'video downloading links from youtube'}, async (message, match) => {
 	
-        const {data} = await axios(`https://api.zeks.me/api/ytplaymp4?apikey=ApiKannappi&q=${match[1]}`)
+        const {data} = await axios(`https://zenzapi.xyz/api/play/playmp4?query=${match[1]}&apikey=Raashii`)
 	
         const { status, result } = data
 
-	var img = await Axios.get(`${result.thumbnail}`, {responseType: 'arraybuffer'})
+	var img = await Axios.get(`${result.url}`, {responseType: 'arraybuffer'})
 
         if(!status) return await message.sendMessage('*NO RESULT FOUND🥲*')
 	
         await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
         let msg = '```'
         msg +=  `TITLE :${result.title}\n\n`
-        msg +=  `THUMBNAIL :${result.thumbnail}\n\n`
-        msg +=  `SOURCE :${result.source}\n\n`
-        msg +=  `SIZE :${result.size}\n\n`
-        msg +=  `DOWNLOADING LINK :${result.url_video}\n\n`
+        msg +=  `THUMBNAIL :${result.thumb}\n\n`
+        msg +=  `CHANNEL :${result.channel}\n\n`
+        msg +=  `PUBLISHED :${result.published}\n\n`
+        msg +=  `VIEWS :${result.views}\n\n`
         msg += '```'
-         return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.image, {mimetype: Mimetype.jpg , caption: msg })
+         return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.video, {mimetype: Mimetype.mp4 , caption: msg })
         });
     
