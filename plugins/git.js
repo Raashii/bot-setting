6cfa4822cab7e2940206927a885336118af31420
Raashii <87118150+Raashii@users.noreply.github.com>
2021-11-05 10:37:13 +0530

    ```fixed```

--- plugins/git.js
+++ plugins/git.js
@@ -12,7 +12,7 @@ Asena.tozara({pattern: 'git', fromMe: wk, desc: 'its send git links'}, (async (m
       let ppUrl
                 try { ppUrl = await conn.getProfilePicture(); } catch { ppUrl = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); }
  
-            const ras = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+            const ras = await axios.get(ppUrl, {responseType: 'arraybuffer'})
             
 
     await message.sendMessage(Buffer.from(ras.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `╭──────────────────╮
