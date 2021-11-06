a27fcf6d2187b36ee72d69013ca32402e0a87ca3
Raashii <87118150+Raashii@users.noreply.github.com>
2021-11-06 09:11:37 +0530

    ```fixed```

--- plugins/extra.js
+++ plugins/extra.js
@@ -3,7 +3,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 const axios = require('axios');
 const config = require('../config');
-
+const axios = require('axios')
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
@@ -115,18 +115,19 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    Rashi.tozara({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api-sv2.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
-	try {
-		const response = await got(url);
-		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*ZaraMwol* 💗' + Lang.BOT +' ```' + json.messages[0].success + '```\n\n' , MessageType.text,{quoted: message.data});
-	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
-	}
-    });
+    Rashi.tozara({ pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC }, async (message, match) => {
+  const tx = " \n\n*Mwolus* 💗 : ```"
+  const { data } = await axios(`https://api-sv2.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`)
 
+  const { success } = data
+    
+  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+  if (match[1] === 'Hi') return await message.reply(tx + 'Hi i am wa userbot```\n\n');
+  if (match[1] === 'hi') return await message.reply(tx + 'Hi i am wa userbot```\n\n');
+
+  await message.client.sendMessage(message.jid, tx + `${success}` + '```\n\n', MessageType.text, { quoted: message.data });
+  
+});
     
 
     Rashi.tozara({pattern: 'quote ?(.*)', fromMe: true, desc: Lang.QUOTE_DESC}, async (message, match) => {
@@ -251,17 +252,19 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    Rashi.tozara({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-	const url = `https://api-sv2.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
-	try {
-		const response = await got(url);
-		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, ' \n\n*Mwolus* 💗' + Lang.BOT +' ```' + json.messages[0].success + '```\n\n' , MessageType.text,{quoted: message.data});
-	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
-	}
-    });
+    Rashi.tozara({ pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC }, async (message, match) => {
+  const tx = " \n\n*Mwolus* 💗 : ```"
+  const { data } = await axios(`https://api-sv2.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`)
+
+  const { success } = data
+    
+  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+  if (match[1] === 'Hi') return await message.reply(tx + 'Hi i am wa userbot```\n\n');
+  if (match[1] === 'hi') return await message.reply(tx + 'Hi i am wa userbot```\n\n');
+
+  await message.client.sendMessage(message.jid, tx + `${success}` + '```\n\n', MessageType.text, { quoted: message.data });
+  
+});
 
     
 
