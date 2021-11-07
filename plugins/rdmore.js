8ff731206541ad39bcab76e8c1f95d6932546ed5
Raashii <87118150+Raashii@users.noreply.github.com>
2021-11-07 19:51:02 +0530

    ```Fixed```

--- plugins/rdmore.js
+++ plugins/rdmore.js
@@ -19,7 +19,7 @@ Asena.tozara({ pattern: 'rdmore ?(.*)', fromMe: false, desc: 'add readmore befor
 }));
 
 
-Asena.tozara({ pattern: 'jid ?(.*)', fromMe: false }, (async (message, match) => {
+Asena.tozara({ pattern: 'jid ?(.*)', fromMe: true }, (async (message, match) => {
 if (message.reply_message === true) return await message.client.sendMessage(message.jid,'🙂', MessageType.text);
 
   await message.client.sendMessage(message.jid, '```JID: ' + message.jid + '```', MessageType.text);
