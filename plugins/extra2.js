c2f622bffef71c7233b88842c3b326227a232b80
Raashii <87118150+Raashii@users.noreply.github.com>
2021-11-09 14:24:36 +0530

    ```🙂```

--- plugins/extra2.js
+++ plugins/extra2.js
@@ -133,6 +133,16 @@ Hisham.tozara({on: 'text', fromMe: false, delownsewcmd: false, onlyPm: true }, (
         await message.sendMessage("𝖡𝗈𝗍 𝗇𝖺𝗆𝖾 𝖼𝗁𝖺𝗇𝗀𝖾𝖽 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 ✅")
     }));
 
+Asena.tozara({ pattern: 'callblock ?(.*)', fromMe: true, desc: 'change mode off call block', usage: '.botname *name* ' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('𝖤𝗇𝗍𝖾𝗋 𝖸𝗈𝗎𝗋 on/off 𝖺𝖿𝗍𝖾𝗋 𝖼𝗈𝗆𝗆𝖺𝗇𝖽')
+        await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['CALL_BLOCK']: match[1]
+            }
+        });
+        await message.sendMessage("```Call block mode``` 𝖼𝗁𝖺𝗇𝗀𝖾𝖽 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 ✅")
+    }));
+
     Asena.tozara({ pattern: 'botlogo ?(.*)', fromMe: true, desc: 'change your bot logo', usage: '.botname *name* ' }, (async (message, match) => {
         if (match[1] == '') return await message.sendMessage('𝗉𝗅𝖾𝖺𝗌𝖾 𝖾𝗇𝗍𝖾𝗋 𝗅𝗂𝗇𝗄 🥴')
         await heroku.patch(baseURI + '/config-vars', {
